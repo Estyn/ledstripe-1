82e573be947803efd3eba47e4ef5456037268f6f
Estyn <estyn@estynedwards.com>
2014-11-01 16:21:01 -0600

    test

--- wellnext.js
+++ wellnext.js
@@ -42,6 +42,16 @@ function processPost(request, response, callback) {
 // 'section': 'all',
 // 'color': {'r':'FF', 'g': 'FF', 'b': '00'}
 // }
+function stringToHex(str) {
+    var hex, i;
+   // var str = "\u6f22\u5b57"; // "\u6f22\u5b57" === "漢字"
+    var result = "";
+    for (i = 0; i < str.length; i++) {
+        hex = str.charCodeAt(i).toString(16);
+        result += ("000" + hex).slice(-4);
+    }
+    return result;
+}
 
 http.createServer(function (request, response) {
     if (request.method == 'POST') {
@@ -52,9 +62,11 @@ http.createServer(function (request, response) {
             //requestData = JSON.parse(request.post);
             requestData = request.post;
             requestData = JSON.parse(request.post);
-
-
-
+            requestData.color.r = stringToHex(requestData.color.r);
+            requestData.color.g = stringToHex(requestData.color.g);
+            requestData.color.b = stringToHex(requestData.color.b);
+console.length(requestData.color.r );
+            
             // everything possibly sane
             myStripeType = 'LPD8806';
             mySpiDevice = '/dev/spidev0.1';
@@ -72,15 +84,14 @@ http.createServer(function (request, response) {
             }
             else
             {
-                console.log(request.post)
-                console.log(request.post.section)
+
                 fillBuffer(aBuf, requestData.section, requestData.color.r, requestData.color.g, requestData.color.b);
             }
             setTimeout(function () {
                 myLedStripe.sendRgbBuf(aBuf);
             }, 1000);
             setTimeout(function () {
-                chaseBuffer(aBuf, 3, 0xFF, 0x00, 0x00);
+                //chaseBuffer(aBuf, 3, 0xFF, 0x00, 0x00);
             }, 1000);
 
             response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
