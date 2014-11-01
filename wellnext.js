64cb480b1c4978fe6c417059c57ab23ba72cf706
Estyn Edwards <estyn@estynedwards.com>
2014-11-01 14:11:50 -0600

    led

--- wellnext.js
+++ wellnext.js
@@ -43,29 +43,29 @@ http.createServer(function (request, response) {
             // connecting to SPI
             myLedStripe.connect(numLEDs, myStripeType, mySpiDevice);
             myLedStripe.fill(0xFF, 0x00, 0x00);
-            
+
 
             var r = 0xFF, g = 0x00, b = 0x00;
             var bufSize = numLEDs * 3;
             var aBuf = new Buffer(bufSize);
-            for (var i = 0; i < 100; i += 3) {
+            for (var i = 0; i < 240; i += 3) {
                 aBuf[i + 0] = r;
                 aBuf[i + 1] = g;
                 aBuf[i + 2] = b;
             }
             r = 0x00;
             g=0xFF;
-            for (var i = 103; i < 200; i += 3) {
+            for (var i = 240; i < 480; i += 3) {
                 aBuf[i + 0] = r;
                 aBuf[i + 1] = g;
                 aBuf[i + 2] = b;
             }
-           // b = 0xFF;
-            //for (var i = 200; i < 240; i += 3) {
-             //   aBuf[i + 0] = r;
-           //     aBuf[i + 1] = g;
-            //    aBuf[i + 2] = b;
-            //}
+            b = 0xFF;
+            for (var i = 480; i < 720; i += 3) {
+               aBuf[i + 0] = r;
+                aBuf[i + 1] = g;
+                aBuf[i + 2] = b;
+            }
             setTimeout(function () {
                 myLedStripe.sendRgbBuf(aBuf);
             });
