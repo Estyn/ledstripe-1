2a53252c3c48dd6d845bf040595fa631dde261be
Estyn Edwards <estyn@estynedwards.com>
2014-11-01 14:03:11 -0600

    led

--- wellnext.js
+++ wellnext.js
@@ -48,7 +48,7 @@ http.createServer(function (request, response) {
                 console.log("green")
             }, 1000);
 
-            var r = 0xFF, g = 0x00, b = 0x00
+            var r = 0xFF, g = 0x00, b = 0x00;
             var bufSize = numLEDs * 3;
             var aBuf = new Buffer(bufSize);
             for (var i = 0; i < 100; i += 3) {
@@ -57,7 +57,7 @@ http.createServer(function (request, response) {
                 aBuf[i + 2] = b;
             }
             r = 0x00;
-            for (var i = 100; i < 200; i += 3) {
+            for (var i = 99; i < 200; i += 3) {
                 aBuf[i + 0] = r;
                 aBuf[i + 1] = g;
                 aBuf[i + 2] = b;
