36b161c6262a182444d8588a088bc1f285182c68
Estyn Edwards <estyn@estynedwards.com>
2014-11-01 13:51:49 -0600

    led

--- wellnext.js
+++ wellnext.js
@@ -47,43 +47,41 @@ http.createServer(function (request, response) {
                 myLedStripe.fill(0x00, 0xFF, 0x00);
                 console.log("green")
             }, 1000);
+
+            var r = 0xFF, g = 0x00, b = 0x00
+            var bufSize = numLEDs * 3;
+            var aBuf = new Buffer(bufSize);
+            for (var i = 0; i < 100; i += 3) {
+                aBuf[i + 0] = r;
+                aBuf[i + 1] = g;
+                aBuf[i + 2] = b;
+            }
+            r = 0x00;
+            for (var i = 100; i < 200; i += 3) {
+                aBuf[i + 0] = r;
+                aBuf[i + 1] = g;
+                aBuf[i + 2] = b;
+            }
+            b = 0xFF;
+            for (var i = 200; i < 240; i += 3) {
+                aBuf[i + 0] = r;
+                aBuf[i + 1] = g;
+                aBuf[i + 2] = b;
+            }
+            setTimeout(function () {
+                myLedStripe.sendRgbBuf(aBuf);
+            });
+
+
+            response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            response.end();
+
         });
-        var r = 0xFF, g = 0x00, b = 0x00
-        var bufSize = numLEDs * 3;
-        var aBuf = new Buffer(bufSize);
-        for (var i = 0; i < 100; i += 3) {
-            aBuf[i + 0] = r;
-            aBuf[i + 1] = g;
-            aBuf[i + 2] = b;
-        }
-        r = 0x00;
-        for (var i = 100; i < 200; i += 3) {
-            aBuf[i + 0] = r;
-            aBuf[i + 1] = g;
-            aBuf[i + 2] = b;
-        }
-        b = 0xFF;
-        for (var i = 200; i < 240; i += 3) {
-            aBuf[i + 0] = r;
-            aBuf[i + 1] = g;
-            aBuf[i + 2] = b;
-        }
-        setTimeout(function () {
-            myLedStripe.sendRgbBuf(aBuf);
-        });
-
-
-
+    }
+    else {
         response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
         response.end();
-
-    
-}
-else
-{
-    response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
-    response.end();
-}
+    }
 
 }).
-listen(8000);
+    listen(8000);
