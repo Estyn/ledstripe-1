a1a1cfc947b52b2eae58ae93fbf9ae04c4bb47ec
Estyn <estyn@estynedwards.com>
2014-11-01 17:24:23 -0600

    sd

--- wellnext.js
+++ wellnext.js
@@ -101,8 +101,8 @@ http.createServer(function (request, response) {
 
                     console.log('off')
                     fillBuffer(aBuf, 1, 0x00, 0x00, 0x00);
-                    fillBuffer(aBuf, 2, 0xFF, 0xFF, 0xFF);
-                    fillBuffer(aBuf, 3, 0xFF, 0xFF, 0xFF);
+                    fillBuffer(aBuf, 2, 0x00, 0x00, 0x00);
+                    fillBuffer(aBuf, 3, 0x00, 0x00, 0x00);
                     myLedStripe.sendRgbBuf(aBuf);
             }
             setTimeout(function () {
